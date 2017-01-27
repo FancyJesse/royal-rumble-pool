6f0479fdd945b7e77cc0c02fec33d82f93795819
FancyJesse <jesus_andrade45@yahoo.com>
2017-01-27 14:30:49 -0800

    Added remove and comment-change option. Options now required

--- royalrumblepool.py
+++ royalrumblepool.py
@@ -33,14 +33,14 @@ def connect():
 		return False
 
 
-# Check if entrant is unassigned
+# Check if entrant exists in database
 def entrant_info(entrant_name):
 	query = 'SELECT * FROM Entrant WHERE Name=?'
 	CURSOR.execute(query, (entrant_name,))
 	return CURSOR.fetchone()
 
 
-# Insert entrant info to database and assign entry number
+# Insert entrant info into database and assign entry number
 def insert_entrant(entrant_name, entrant_comment=None):
 	entrant_name = entrant_name.strip()
 	if not entrant_name:
@@ -54,8 +54,36 @@ def insert_entrant(entrant_name, entrant_comment=None):
 		CURSOR.execute(query, (entrant_name, entry_number, entrant_comment))
 		DATABASE.commit()
 		return True, '{} has entered the Royal Rumble as #{}!'.format(entrant_name, entry_number)
-
 	return False, '{} has already been assigned Entry Number #{} on {}!'.format(entrant[0], entrant[1], entrant[3])
+
+
+# Remove entrant from database
+def remove_entrant(entrant_name):
+	entrant_name = entrant_name.strip()
+	if not entrant_name:
+		return False, 'Invalid Entry Name'
+	if entrant_info(entrant_name):
+		query = 'DELETE FROM Entrant WHERE Name=?'
+		CURSOR.execute(query, (entrant_name,))
+		DATABASE.commit()
+		return True, '{} has been removed the Royal Rumble!'.format(entrant_name)
+	return False, '{} is not in the Royal Rumble!'.format(entrant_name)
+
+
+# Change entrant's comment in database
+def update_comment(entrant_name, new_comment):
+	entrant_name = entrant_name.strip()
+	if not entrant_name:
+		return False, 'Invalid Entry Name'
+	if new_comment:
+		new_comment = new_comment.strip()
+	entrant = entrant_info(entrant_name)
+	if entrant_info(entrant_name):
+		query = 'UPDATE Entrant SET Comment=? WHERE Name=?'
+		CURSOR.execute(query, (new_comment, entrant_name))
+		DATABASE.commit()
+		return True, "{}'s comment has been altered: '{}' -> '{}'".format(entrant_name, entrant[2], new_comment)
+	return False, '{} is not in the Royal Rumble!'.format(entrant_name)
 
 
 # Get all database content
@@ -74,13 +102,19 @@ def dump():
 # Ran through console
 if __name__ == '__main__':
 	args = sys.argv[1:]
-	result = False, 'Invalid Arguments - Required [entrant_name] [entrant_comment]'
+	result = False, 'Invalid Arguments - Required [OPTION] [ARG]*'
 	if args:
 		if connect():
 			if len(args) == 1 and args[0] == '-d':
 				result = dump()
 			elif len(args) == 2:
-				result = insert_entrant(args[0], args[1])
+				if args[0] == '-remove':
+					result = remove_entrant(args[1])
+			elif len(args) == 3:
+				if args[0] == '-add':
+					result = insert_entrant(args[1], args[2])
+				elif args[0] == '-comment':
+					result = update_comment(args[1], args[2])
 		else:
 			result = False, 'Database Connection Failed.'
 	print(json.dumps({'success':result[0], 'data':result[1]}))
